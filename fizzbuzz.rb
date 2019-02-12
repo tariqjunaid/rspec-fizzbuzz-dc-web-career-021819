ca265c2f70b3bec50732d6100e96807f914a82c8
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-12 00:13:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
     return "FizzBuzz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 3
+  elsif int % 3 == 0
     return "Fizz"
+  elsif int % 5
+    return "Buzz"
   else
     return nil
   end
